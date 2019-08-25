604704e1935889c6a6790fd280ae12eb4d00f299
glicodin21 <54497751+glicodin21@users.noreply.github.com>
2019-08-25 22:55:28 +0500

    Delete some_func.py

--- some_func.py
+++
@@ -1,8 +0,0 @@
-def sum_two_lowest_numbers(numbers):
-    return sum(sorted(numbers)[:2])
-
-
-def sum_two_lowest_numbers_with_len_validation(numbers):
-    if len(numbers) < 2:
-        raise ValueError("Not enough numbers")
-    return sum(sorted(numbers)[:2])
